b90f3d8adeeb73b9841b8d1df564866a0a1768f2
g33ya <gscozzaro2004@gmail.com>
2023-01-17 22:57:53 -0500

    shakeshake

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 // All possible 8ball answers
-let answers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes definitely.", "You may rely on it.", 
-"As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Ask again later.", "Better not tell you now.",
-"Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
+let answers = ["IT IS CERTAIN.", "IT IS DECIDEDLY SO.", "WITHOUT A DOUBT.", "YES DEFINITELY.", "YOU MAY RELY ON IT.", 
+"AS I SEE IT, YES.", "MOST LIKELY.", "OUTLOOK GOOD.", "YES.", "SIGNS POINT TO YES.", "ASK AGAIN LATER.", "BETTER NOT TELL YOU NOW.",
+"CONCENTRATE AND ASK AGAIN.", "DON'T COUNT ON IT.", "MY REPLY IS NO.", "MY SOURCES SAY NO.", "OUTLOOK NOT SO GOOD.",
+ "VERY DOUBTFUL."]
 
 function givenAnswer() {
     let response = answers[Math.floor(Math.random() * 17)];
     let answer = document.querySelector(".answer");
     answer.textContent = response;
     console.log(response);
+    reset_animation_text();
+    reset_animation_ball()
 }  
 
 document.querySelector('.ballbutton').addEventListener('click', givenAnswer);
 document.querySelector('.text').addEventListener('click', givenAnswer);
+
+function reset_animation_text() {
+    var el = document.getElementById('textBox');
+    el.style.animation = 'none';
+    el.offsetHeight; /* trigger reflow */
+    el.style.animation = null; 
+  }
+  function reset_animation_ball() {
+    var el = document.getElementById('ballAni');
+    el.style.animation = 'none';
+    el.offsetHeight; /* trigger reflow */
+    el.style.animation = null; 
+  }
 
 
