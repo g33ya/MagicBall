cfee3fb4ca216c1d2d06823f5369de09ceb0fade
g33ya <gscozzaro2004@gmail.com>
2023-01-24 13:52:41 -0500

    bugs!

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // All possible 8ball answers
 let answers = ["IT IS CERTAIN.", "IT IS DECIDEDLY SO.", "WITHOUT A DOUBT.", "YES DEFINITELY.", "YOU MAY RELY ON IT.", 
 "AS I SEE IT, YES.", "MOST LIKELY.", "OUTLOOK GOOD.", "YES.", "SIGNS POINT TO YES.", "ASK AGAIN LATER.", "BETTER NOT TELL YOU NOW.",
-"CONCENTRATE AND ASK AGAIN.", "DON'T COUNT ON IT.", "MY REPLY IS NO.", "MY SOURCES SAY NO.", "OUTLOOK NOT SO GOOD.",
+"FOCUS AND ASK AGAIN.", "DON'T COUNT ON IT.", "MY REPLY IS NO.", "MY SOURCES SAY NO.", "OUTLOOK NOT SO GOOD.",
  "VERY DOUBTFUL."]
 
+ // Retrieves a random index of the "answers" list (above) and resets ball animations
 function givenAnswer() {
     let response = answers[Math.floor(Math.random() * 17)];
     let answer = document.querySelector(".answer");
@@ -13,9 +14,12 @@ function givenAnswer() {
     reset_animation_ball()
 }  
 
+// Connects givenAnswer() to click function, retrieving the random index text and displaying it through the .text class (line 43 in index.html)
 document.querySelector('.ballbutton').addEventListener('click', givenAnswer);
 document.querySelector('.text').addEventListener('click', givenAnswer);
 
+
+// Allows animations to repeat on each click
 function reset_animation_text() {
     var el = document.getElementById('textBox');
     el.style.animation = 'none';
